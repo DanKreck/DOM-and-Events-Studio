17478d5b6b899cd8c2250d8a7d295ff31664e4bd
Daniel Kreck <Daniel_Kreck@comcast.com>
2022-07-21 16:41:41 -0400

    Finished Studio

--- scripts.js
+++ scripts.js
@@ -13,6 +13,11 @@ window.addEventListener("load", function() {
     let currentHeight = Number(shuttleHeight.innerHTML);
     let rocketNode = this.document.getElementById("rocket");
 
+    let upBtn = document.getElementById("up");
+    let downBtn = document.getElementById("down");
+    let leftBtn = document.getElementById("left");
+    let rightBtn = document.getElementById("right");
+
     takeoffButton.addEventListener('click', event => {
         let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if(response){
@@ -30,20 +35,40 @@ window.addEventListener("load", function() {
         let response = window.alert("The shuttle is landing. Landing gear engaged.");
         status.innerHTML = "The shuttle has landed.";
         background.style.backgroundColor = "green";
-        
+        //Update shuttle 
         shuttleHeight.innerHTML = 0;
         rocketNode.style.bottom = "0px";
-
-
-
     });
 
     abortButton.addEventListener('click', event => {
-        let response = window.alert("The shuttle is landing. Landing gear engaged.");
-        status.innerHTML = "The shuttle has landed.";
+        let response = window.alert("Confirm that you want to abort the mission.");
+        status.innerHTML = "Mission aborted.";
         background.style.backgroundColor = "green";
-        shuttleHeight.innerHTML = "0"; 
+        shuttleHeight.innerHTML = 0;
+        rocketNode.style.bottom = "0px";
 
     });
 
+    
+    upBtn.addEventListener('click', event => {
+        let currentImgHeight = parseInt(rocketNode.style.bottom);
+        rocketNode.style.bottom = (currentImgHeight + 10) + "px";
+    });
+    downBtn.addEventListener('click', event => {
+        let currentImgHeight = parseInt(rocketNode.style.bottom);
+        rocketNode.style.bottom = (currentImgHeight - 10) + "px";
+        console.log(rocketNode.style.bottom);
+    });
+    leftBtn.addEventListener('click', event => {
+        let currentImgPos = parseInt(rocketNode.style.left);
+        rocketNode.style.left = (currentImgPos - 10) + "px";
+        
+        console.log(rocketNode.style.left);
+
+    });
+    rightBtn.addEventListener('click', event => {
+        let currentImgPos = parseInt(rocketNode.style.left);
+        rocketNode.style.left = (currentImgPos + 10) + "px";
+
+    });
 });
